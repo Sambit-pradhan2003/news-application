346ff5c417a9d7aef2d2b8533b97050c4201f318
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-03-16 11:51:08 +0530

    backend utility file changed

---
+++ backend/src/utils/asynchandler.js
@@ -0,0 +1,9 @@
+const asynchandaler=(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err))
+    }
+}
+ 
+
+export {asynchandaler}
